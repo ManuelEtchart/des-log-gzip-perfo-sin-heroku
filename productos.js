8211b84ea952b517343e4b9ab14a551ad74d20c8
Manuel Etchart <manuetchart@live.com.ar>
2022-05-09 16:51:45 -0300

    commit inicial

---
+++ productos.js
@@ -0,0 +1,87 @@
+import express from 'express';
+import { mensajesMonDB } from './mensajes.js';
+import { loggerError, logger } from './server.js';
+import ProductosDaoMongoDB from './src/DAOs/productosDaoMongoDB'
+
+const productos = express.Router();
+
+export const productosMongoDB = new ProductosDaoMongoDB();
+
+productos.use(express.json());
+productos.use(express.urlencoded({extended: true}));
+
+const administrador = true;
+
+productos.get('/form', async (req,res)=>{
+    logger.info(`ruta ${req.url} metodo ${req.method} implementada`)
+    res.render('productosForm', {mensajes: await mensajesMonDB.getAll()});
+});
+
+productos.get('/:id?', async (req,res) => {
+    logger.info(`ruta ${req.url} metodo ${req.method} implementada`)
+    try {
+        if (req.params.id === undefined) {
+            res.render('inicio', {productos: await productosMongoDB.getAll(), mensajes: await mensajesMonDB.getAll()})
+        }else{
+            res.render('producto',{producto: await productosMongoDB.getById(req.params.id), mensajes: await mensajesMonDB.getAll()})
+        }
+    } catch (error) {
+        loggerError.error(`${error} - Hubo un error en ruta ${req.url} metodo ${req.method} implementada`)
+    }
+});
+
+productos.post('', async (req,res) => {
+    logger.info(`ruta ${req.url} metodo ${req.method} implementada`)
+    try{
+        if(administrador){
+            
+            await productosMongoDB.save({
+                timestamp: Date.now(),
+                nombre: req.body.nombre,
+                descripcion: req.body.descripcion,
+                codigo: req.body.codigo,
+                foto: req.body.urlFoto,
+                precio: req.body.precio,
+                stock: req.body.stock
+            });
+
+            res.redirect('/')
+            
+        }else{
+            loggerError.error(`${error} - Hubo un error en ruta ${req.url} metodo ${req.method} implementada - Ruta no autorizada`)
+            res.send({error: '-1', descripcion: `ruta ${req.url} metodo ${req.method} no autorizada`});
+        }
+    }catch(error){
+        loggerError.error(`${error} - Hubo un error en ruta ${req.url} metodo ${req.method} implementada`);
+    }
+});
+
+productos.put('/:id', async (req,res) => {
+    logger.info(`ruta ${req.url} metodo ${req.method} implementada`)
+    try {
+        if(administrador){
+            res.json(await productosMongoDB.updateById(req.params.id, req.query));
+        }else{
+            loggerError.error(`${error} - Hubo un error en ruta ${req.url} metodo ${req.method} implementada - Ruta no autorizada`)
+            res.send({error: '-1', descripcion: `ruta ${req.url} metodo ${req.method} no autorizada`});
+        }
+    } catch (error) {
+        loggerError.error(`${error} - Hubo un error en ruta ${req.url} metodo ${req.method} implementada`)
+    }
+})
+
+productos.delete('/:id', async (req,res) => {
+    logger.info(`ruta ${req.url} metodo ${req.method} implementada`)
+    try {
+        if(administrador){
+            res.json(await productosMongoDB.deleteById(req.params.id))
+        }else{
+            loggerError.error(`${error} - Hubo un error en ruta ${req.url} metodo ${req.method} implementada - Ruta no autorizada`)
+            res.send({error: '-1', descripcion: `ruta ${req.url} metodo ${req.method} no autorizada`});
+        }
+    } catch (error) {
+        loggerError.error(`${error} - Hubo un error en ruta ${req.url} metodo ${req.method} implementada`)
+    }
+})
+
+export default productos;
---
+++ server.js
@@ -0,0 +1,140 @@
+import express from 'express';
+import minimist from 'minimist';
+import compression from 'compression';
+import { cpus } from 'os';
+import path from 'path';
+import log4js from 'log4js';
+import hbs from 'express-handlebars';
+import session from 'express-session';
+import cookieParser from 'cookie-parser';
+import productos from './productos.js';
+import carrito from './carrito.js';
+import mensajes, { mensajesMemoria } from './mensajes.js';
+
+const app = express();
+
+app.use(compression());
+//app.use(express.static('public'));
+
+log4js.configure({
+    appenders:{
+        logConsola: {type: 'console'},
+        logError: {type: 'file', filename: 'error.log'},
+        logWarn: {type: 'file', filename: 'warn.log'},
+        loggerWarn: {type: 'logLevelFilter', appender: 'logWarn', level: 'warn'},
+        loggerError: {type: 'logLevelFilter', appender: 'logError', level: 'error'}
+        
+    },
+    categories:{
+        default:{
+            appenders: ["loggerError", "logConsola"], level: "all"
+        },
+        custom:{
+            appenders: ["loggerWarn", "logConsola"], level: "all"
+        }
+    }
+});
+
+export const logger = log4js.getLogger('custom');
+export const loggerError = log4js.getLogger();
+
+app.set('views', path.join(path.dirname(''), 'src/views'));
+
+app.engine('.hbs', hbs.engine({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+}))
+
+app.set('view engine', '.hbs');
+
+app.use(cookieParser())
+app.use(session({
+   secret: '123456789!#$%&/()',
+   resave: false,
+   saveUninitialized: false,
+   cookie: {
+      secure: 'auto',
+      maxAge: 600000
+   }
+}));
+
+let options = {alias: {p: 'puerto'}};
+let args = minimist(process.argv, options);
+
+app.use('/api/carrito', carrito);
+app.use('/api/productos', productos);
+app.use('/api/mensajes', mensajes);
+
+app.get('/', (req,res)=>{
+    logger.info(`ruta ${req.url} metodo ${req.method} implementada`)
+    res.redirect('/api/productos')
+});
+
+app.get('/api/info', async (req,res)=>{
+    logger.info(`ruta ${req.url} metodo ${req.method} implementada`)
+    try{
+        console.log({
+            cpus: cpus.length,
+            argsEnt: process.argv.slice(2),
+            nomPlat: process.platform,
+            verNode: process.version,
+            memToRev: JSON.stringify(process.memoryUsage().rss),
+            pathExe: process.execPath,
+            procId: process.pid,
+            carProy: process.cwd()
+        });
+        res.render('info',{
+            datos:{
+                cpus: cpus.length,
+                argsEnt: process.argv.slice(2),
+                nomPlat: process.platform,
+                verNode: process.version,
+                memToRev: JSON.stringify(process.memoryUsage().rss),
+                pathExe: process.execPath,
+                procId: process.pid,
+                carProy: process.cwd()
+            },
+            mensajes: await mensajesMemoria.getAll()
+        });
+    } catch (error) {
+        loggerError.error(`${error} - Hubo un error en ruta ${req.url} metodo ${req.method} implementada`)
+    }
+});
+
+/*app.get('/api/randoms', (req,res) => {
+    logger.info(`ruta ${req.url} metodo ${req.method} implementada`)
+    console.log('no bloqueante antes')
+    const randoms = fork('./random.js')
+    randoms.send({query: req.query.cant})
+    randoms.on('message', randoms =>{
+       res.render('random', {random: JSON.stringify(randoms)})
+    })
+    console.log('no bloqueante despues')
+})
+*/
+app.get('*', (req,res) => {
+    res.send({error: '-2', descripcion: `ruta ${req.url} metodo ${req.method} no implementada`});
+    logger.warn({error: '-2', descripcion: `ruta ${req.url} metodo ${req.method} no implementada`})
+});
+app.post('*', (req,res) => {
+    res.send({error: '-2', descripcion: `ruta ${req.url} metodo ${req.method} no implementada`});
+    logger.warn({error: '-2', descripcion: `ruta ${req.url} metodo ${req.method} no implementada`})
+});
+app.delete('*', (req,res) => {
+    res.send({error: '-2', descripcion: `ruta ${req.url} metodo ${req.method} no implementada`});
+    logger.warn({error: '-2', descripcion: `ruta ${req.url} metodo ${req.method} no implementada`})
+});
+app.put('*', (req,res) => {
+    res.send({error: '-2', descripcion: `ruta ${req.url} metodo ${req.method} no implementada`});
+    logger.warn({error: '-2', descripcion: `ruta ${req.url} metodo ${req.method} no implementada`})
+});
+
+const PORT = process.env.PORT || 8080;
+
+const server = app.listen(PORT, () => {
+   logger.info(`Servidor escuchando en el puerto ${server.address().port}`);
+});
+
+server.on("error", error => loggerError.error(error, `Error en servidor ${error}`) ); 
